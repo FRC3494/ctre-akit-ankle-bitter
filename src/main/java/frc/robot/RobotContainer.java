8dfa6a2dfa92fa2a2dcc9eb3df299281212102a4
EagleEye2649 <oliverg2649@gmail.com>
2026-08-17 21:01:35 -0400

    Set gyro re-zero button to start

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -153,7 +153,7 @@ public class RobotContainer {
 
     // Reset gyro to 0° when B button is pressed
     controller
-        .back()
+        .start()
         .onTrue(
             Commands.runOnce(
                     () ->
