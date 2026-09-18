82cb041a2b0608cd17ff2d3a490c1e2dc969fb8c
liujip0 <liujip2020@gmail.com>
2026-09-17 21:33:28 -0400

    turn on choreo codegen

---
+++ src/main/java/frc/robot/generated/ChoreoVars.java
@@ -0,0 +1,21 @@
+// spotless:off
+package frc.robot.generated;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.*;
+
+/**
+ * Generated file containing variables defined in Choreo.
+ * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
+ * in the Choreo GUI.
+ */
+public final class ChoreoVars {
+
+
+    public static final class Poses {
+        public static final Pose2d Origin = new Pose2d(5, 0, Rotation2d.fromRadians(0));
+        public static final Pose2d startingPosition = new Pose2d(3.5471871, 4.0375147, Rotation2d.fromRadians(0));
+    }
+}
+// spotless:on
